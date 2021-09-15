7cadb766d0c2c06940873f7d34ac5f94c28a1ce4
MadaraProgrammee <madara.lauga@gmail.com>
2021-09-15 19:42:50 +0300

    ok

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,7 +6,38 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+           
+
+
+
+            //--------1.uzdevums--------------------
+
+            Console.WriteLine("------UZDEVUMS #1------");
+
+            Console.WriteLine("Kā Tevi sauc?");
+
+            string userName1 = Console.ReadLine();
+
+            Console.WriteLine("Sveiks, " + userName1 + "!");
+
+
+            //--------2.uzdevums--------------------
+
+            Console.WriteLine("------UZDEVUMS #2------");
+
+            Console.WriteLine("Kāds ir Tavs vecums?");
+
+            string userAge = Console.ReadLine();
+            int age = int.Parse(userAge);
+
+            int ageNextYear = userAge + 1;
+            bool isAdult1 = userAge >= 18;
+
+
+            Console.WriteLine("Tavs vecums ir " + userAge + "nākamgad Tev paliks "+ ageNextYear + "Tu esi pieaugušais" + isAdult1);
+
+
+
         }
     }
 }
